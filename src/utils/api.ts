781953b8188d314289ba704d6fd89fa05133724b
Carlos Ruana Linares <carlosruana@gmail.com>
2025-02-28 15:16:12 +0100

    Updating api calls

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,33 +3,35 @@ import { Wine } from '../types/wine';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const getAuthHeaders = () => {
+  // Only add the Authorization header if an API_KEY is provided
+  return API_KEY ? { 'Authorization': `Bearer ${API_KEY}` } : {};
+};
   
 export const getWineDetails = async (name: string): Promise<any[]> => {
-	const response = await axios.get(`${API_URL}/wines/details/${name}`);
+	const response = await axios.get(`${API_URL}/wine-details/${name}`);
 	return response.data;
   };
 
 export const getFavorites = async (): Promise<Wine[]> => {
-  const response = await axios.get(`${API_URL}/favorites`, {
-    headers: {
-      'Authorization': `Bearer ${API_KEY}`,
-    },
+  const response = await axios.get(`${API_URL}/wine-favorites`, {
+    headers: getAuthHeaders()
   });
   return response.data;
 };
 
 export const toggleFavorite = async (wine: Wine): Promise<void> => {
-  await axios.post(`${API_URL}/favorites`, wine, {
-    headers: {
-      'Authorization': `Bearer ${API_KEY}`,
-    },
+  await axios.post(`${API_URL}/wine-favorites`, wine, {
+    headers: getAuthHeaders()
   });
 };
 
 export const analyzeImage = async (file: File): Promise<{ wineName: string }> => {
 	const formData = new FormData();
 	formData.append('image', file);
-	const response = await axios.post(`${API_URL}/analyze`, formData, {
+
+	const response = await axios.post(`${API_URL}/image-analysis`, formData, {
 	  headers: {
 		'Content-Type': 'multipart/form-data',
 	  },
